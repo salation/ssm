4c6fc6d8badaa087c0dccc5b51aebcc7c2a2a686
luyiheng <luyiheng@inspur.com>
2019-09-24 11:56:12 +0800

    修改时间

--- src/main/java/controller/RequestTestController.java
+++ src/main/java/controller/RequestTestController.java
@@ -10,6 +10,6 @@ public class RequestTestController {
 
     @GetMapping()
     public String TestString(){
-        return "this is a test string. Time:2017-10-29 20:42:00";
+        return "this is a test string. Time:2019-10-29 20:42:00";
     }
 }
